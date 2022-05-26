ee3800a9d40e3e38dab89f660e7bd6d7a49f4762
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2022-05-26 14:17:55 +0900

    Add tests

---
+++ test/data_pool.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+final List<DateTimeRange> data1 = [
+  DateTimeRange(
+    start: DateTime(2021, 10, 22, 23, 55),
+    end: DateTime(2021, 10, 23, 8, 0),
+  ),
+  DateTimeRange(
+    start: DateTime(2021, 10, 21, 21, 15),
+    end: DateTime(2021, 10, 22, 6, 10),
+  ),
+];
+
+final List<DateTimeRange> data2 = [
+  DateTimeRange(
+    start: DateTime(2021, 2, 2, 18, 4),
+    end: DateTime(2021, 2, 3, 8, 0),
+  ),
+  DateTimeRange(
+    start: DateTime(2021, 2, 1, 15, 3),
+    end: DateTime(2021, 2, 2, 5, 4),
+  ),
+];
+
+final List<DateTimeRange> data3 = [
+  DateTimeRange(
+    start: DateTime(2021, 2, 3, 20, 42),
+    end: DateTime(2021, 2, 4, 12, 10),
+  ),
+  DateTimeRange(
+    start: DateTime(2021, 2, 2, 18, 4),
+    end: DateTime(2021, 2, 3, 8, 0),
+  ),
+  DateTimeRange(
+    start: DateTime(2021, 2, 1, 15, 3),
+    end: DateTime(2021, 2, 2, 5, 4),
+  ),
+];
